5a57b6fc811141915a72c271f6d7b68e689a8f75
agata1805 <agacia1805@gmail.com>
2021-06-03 12:22:18 +0200

    Add action to the files

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Load a book from disk
-function loadBook(filename, displayName) {
+function loadBook(fileName, displayName) {
     let currentBook = '';
-    let url = 'books/' + filename;
+    let url = 'books/' + fileName;
 
     // reset out UI
     document.getElementById('fileName').innerHTML = displayName;
@@ -13,11 +13,11 @@ function loadBook(filename, displayName) {
     xhr.open('GET', url, true); // asynchronous to check !!!!!!!!
     xhr.send();
 
-    xhr.onreadystatechange = function() {
-        if(xhr.readyState === 4 && xhr.status === 200) {   //CHECK
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {   //CHECK
             currentBook = xhr.responseText;
-
-            getDocStats(currentBook);
+            //
+            //         getDocStats(currentBook);
 
             // remove line breaks and carriage (regular expressions)
             currentBook = currentBook.replace(/(?:\r\n|\r|\n)/g, '<br>');
@@ -28,90 +28,128 @@ function loadBook(filename, displayName) {
             elmnt.scrollTop = 0;
         }
     }; // !!!!!!! to check
-}
 
-// get the stats for the book
-function getDocStats(fileContent) {
-    let docLength = document.getElementById('docLength');
-    let wordCount = document.getElementById('wordCount');
-    let charCount = document.getElementById('charCount');
+//
+// // get the stats for the book
+// function getDocStats(fileContent) {
+//     let docLength = document.getElementById('docLength');
+//     let wordCount = document.getElementById('wordCount');
+//     let charCount = document.getElementById('charCount');
+//
+//     let text = fileContent.toLowerCase();
+//     let wordArray = text.match(/\b\S+\b/g);
+//     let wordDictionary = {};
+//
+//     let uncommonWords = [];
+//
+//     // filter out the uncommon words
+//     uncommonWords = filterStopWords(wordArray);
+//
+//     // count every word in the wordArray
+//     for (let word in uncommonWords) {
+//         let wordValue = uncommonWords[word];
+//         if (wordDictionary[wordValue] > 0) {
+//             wordDictionary[wordValue] += 1;
+//         }
+//         else {
+//             wordDictionary[wordValue] = 1;
+//         }
+//     }
+//
+//     // sort the array
+//     let wordList = sortProperties(wordDictionary);
+//
+//     // return the top five words
+//     let top5Words = wordList.slice(0, 6);
+//     let least5Words = wordList.slice(-6, wordList.length);
+//
+//     // write the values to the page
+//     ULTemplate(top5Words, document.getElementById('mostUsed'));
+//     ULTemplate(least5Words, document.getElementById('leastUSed'));
+//
+//     docLength.innerText = ' Document Length: ' + text.length;
+//     wordCount.innerText = 'Word Count: ' + wordArray.length;
+//
+// }
+//
+// function ULTemplate(items, element) {
+//     let rowTemplate = document.getElementById('template-ul-items');
+//     let templateHTML = rowTemplate.innerHTML;
+//     let resultsHTML = '';
+//
+//     for (let i = 0; i < items.length - 1; i++) {
+//         resultsHTML += templateHTML.replace('{{val}}', items[i][0] + ': ' + items[i][1] + ' time(s)');
+//     }
+//
+//     element.innerHTML = resultsHTML;
+// }
+//
+//
+// function sortProperties(obj) {
+//     // convert the object to an array
+//     let rtnArray = Object.entries(obj);
+//     // sort the array based
+//     rtnArray.sort(function (first, second) {
+//         return second[1] - first[1];
+//     });
+//     return rtnArray;
+// }
+//
+// function filterStopWords(wordArray) {
+//     let commonWords = getStopWords();
+//     let commonObj = {};
+//     let uncommonArr = [];
+//
+//     for (let i = 0; i < commonWords.length; i++) {
+//         commonObj[commonWords[i].trim()] = true;
+//     }
+//
+//     for (let i = 0; i < wordArray.length; i++) {
+//         word = wordArray[i].trim().toLowerCase();
+//         if (!commonObj[word]) {
+//             uncommonArr.push(word);
+//         }
+//     }
+//     return uncommonArr;
+// }
+//
+// function getStopWords() {
+//     return ["a", "able", "about", "across", "after", "all", "almost", "also", "am", "among", "an", "and", "any", "are", "as", "at", "be", "because", "been", "but", "by", "can", "cannot", "could", "dear", "did", "do", "does", "either", "else", "ever", "every", "for", "from", "get", "got", "had", "has", "have", "he", "her", "hers", "him", "his", "how", "however", "i", "if", "in", "into", "is", "it", "its", "just", "least", "let", "like", "likely", "may", "me", "might", "most", "must", "my", "neither", "no", "nor", "not", "of", "off", "often", "on", "only", "or", "other", "our", "out", "down", "up", "own", "rather", "said", "say", "says", "she", "should", "since", "so", "some", "than", "that", "the", "their", "them", "then", "there", "these", "they", "this", "tis", "to", "too", "twas", "us", "wants", "was", "we", "were", "what", "when", "where", "which", "while", "who", "whom", "why", "will", "with", "would", "yet", "you", "your", "ain't", "aren't", "can't", "could've", "couldn't", "didn't", "doesn't", "don't", "hasn't", "he'd", "he'll", "he's", "how'd", "how'll", "how's", "i'd", "i'll", "i'm", "i've", "isn't", "it's", "might've", "mightn't", "must've", "mustn't", "shan't", "she'd", "she'll", "she's", "should've", "shouldn't", "that'll", "that's", "there's", "they'd", "they'll", "they're", "they've", "wasn't", "we'd", "we'll", "we're", "weren't", "what'd", "what's", "when'd", "when'll", "when's", "where'd", "where'll", "where's", "who'd", "who'll", "who's", "why'd", "why'll", "why's", "won't", "would've", "wouldn't", "you'd", "you'll", "you're", "you've"];
+// }
+//
+// // highlight words in the search
+//
+    function performMark() {
+        // read the keyword
+        let keyword = document.getElementById('keyword').value;
+        let display = document.getElementById('fileContent');
 
-    let text = fileContent.toLowerCase();
-    let wordArray = text.match(/\b\S+\b/g);
-    let wordDictionary = {};
+        let newContent = '';
 
-    let uncommonWords = [];
+        // find all the currently marked items
 
-    // filter out the uncommon words
-    uncommonWords = filterStopWords(wordArray);
+        let spans = document.querySelectorAll('mark');
 
-    // count every word in the wordArray
-    for (let word in uncommonWords) {
-        let wordValue = uncommonWords[word];
-        if (wordDictionary[wordValue] > 0) {
-            wordDictionary[wordValue] += 1;
+        for (let i = 0; i < spans.length; i++) {
+            spans[i].outerHTML = spans[i].innerHTML;
         }
-        else {
-            wordDictionary[wordValue] = 1;
+
+        let re = new RegExp(keyword, 'g');
+        let replaceText = '<mark id = "markme">$&</mark>';
+        let bookContent = display.innerHTML;
+
+        newContent = bookContent.replace(re, replaceText);
+
+        display.innerHTML = newContent;
+        let count = document.querySelectorAll('mark').length;
+        document.getElementById('searchstat').innerHTML = 'found ' + count + ' matches';
+
+
+        if (count > 0) {
+            let element = document.getElementById('markme');
+            element.scrollIntoView();
         }
+
     }
-
-    // sort the array
-    let wordList = sortProperties(wordDictionary);
-
-    // return the top five words
-    let top5Words = wordList.slice(0, 6);
-    let least5Words = wordList.slice(-6, wordList.length);
-
-    // write the values to the page
-    ULTemplate(top5Words, document.getElementById('mostUsed'));
-    ULTemplate(least5Words, document.getElementById('leastUSed'));
 }
-
-function ULTemplate(items, element) {
-    let rowTemplate = document.getElementById('template-ul-items');
-    let templateHTML = rowTemplate.innerHTML;
-    let resultsHTML = '';
-
-    for (let i = 0; i < items.length - 1; i++) {
-        resultsHTML += templateHTML.replace('{{val}}', items[i][0] + ': ' + items[i][1] + ' time(s)');
-    }
-
-    element.innerHTML = resultsHTML;
-}
-
-
-function sortProperties(obj) {
-    // convert the object to an array
-    let rtnArray = Object.entries(obj);
-    // sort the array based
-    rtnArray.sort(function (first, second) {
-        return second[1] - first[1];
-    });
-    return rtnArray;
-}
-
-function filterStopWords(wordArray) {
-    let commonWords = getStopWords();
-    let commonObj = {};
-    let uncommonArr = [];
-
-    for (let i = 0; i < commonWords.length; i++) {
-        commonObj[commonWords[i].trim()] = true;
-    }
-
-    for (let i = 0; i < wordArray.length; i++) {
-        word = wordArray[i].trim().toLowerCase();
-        if (!commonObj[word]) {
-            uncommonArr.push(word);
-        }
-    }
-    return uncommonArr;
-}
-
-function getStopWords() {
-    return ["a", "able", "about", "across", "after", "all", "almost", "also", "am", "among", "an", "and", "any", "are", "as", "at", "be", "because", "been", "but", "by", "can", "cannot", "could", "dear", "did", "do", "does", "either", "else", "ever", "every", "for", "from", "get", "got", "had", "has", "have", "he", "her", "hers", "him", "his", "how", "however", "i", "if", "in", "into", "is", "it", "its", "just", "least", "let", "like", "likely", "may", "me", "might", "most", "must", "my", "neither", "no", "nor", "not", "of", "off", "often", "on", "only", "or", "other", "our", "own", "rather", "said", "say", "says", "she", "should", "since", "so", "some", "than", "that", "the", "their", "them", "then", "there", "these", "they", "this", "tis", "to", "too", "twas", "us", "wants", "was", "we", "were", "what", "when", "where", "which", "while", "who", "whom", "why", "will", "with", "would", "yet", "you", "your", "ain't", "aren't", "can't", "could've", "couldn't", "didn't", "doesn't", "don't", "hasn't", "he'd", "he'll", "he's", "how'd", "how'll", "how's", "i'd", "i'll", "i'm", "i've", "isn't", "it's", "might've", "mightn't", "must've", "mustn't", "shan't", "she'd", "she'll", "she's", "should've", "shouldn't", "that'll", "that's", "there's", "they'd", "they'll", "they're", "they've", "wasn't", "we'd", "we'll", "we're", "weren't", "what'd", "what's", "when'd", "when'll", "when's", "where'd", "where'll", "where's", "who'd", "who'll", "who's", "why'd", "why'll", "why's", "won't", "would've", "wouldn't", "you'd", "you'll", "you're", "you've"];
-}
-
-
 
